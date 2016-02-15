e652669e8879e2a1fa960393d983307f5a814e63
Your Name <you@example.com>
2016-02-15 17:55:13 -0600

    agregando una nueva ruta para OSX

--- instalaciones/instalacion_paquetes_sublime.sh
+++ instalaciones/instalacion_paquetes_sublime.sh
@@ -3,9 +3,10 @@
 clear
 echo "=========================================================="
 echo "Instalando paquetes de sublime"
-cd ~/.config/sublime-text-2/Packages
+# cd ~/.config/sublime-text-2/Packages
 # cd ~/.config/sublime-text-3/Packages
 # cd ~/.Sublime\ Text\ 2/Packages/
+cd ~/Library/Application\ Support/Sublime\ Text\ 2/Packages
 sudo git clone https://github.com/Xavura/CoffeeScript-Sublime-Plugin.git CoffeeScript
 sudo git clone https://github.com/billymoon/Stylus
 sudo git clone https://github.com/davidrios/jade-tmbundle.git Jade
